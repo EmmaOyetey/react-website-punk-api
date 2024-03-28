25a6e64234d4986d3336b1889311096b68ce8fc6
Emma Oyetey <emmaflookis@hotmail.com>
2024-03-28 10:23:38 +0000

    styling added to BeerCards in BeerCards.scss

--- src/Components/BeerCard/BeerCard.tsx
+++ src/Components/BeerCard/BeerCard.tsx
@@ -1,23 +1,35 @@
-//import "./BeerCard.scss"
+import "./BeerCard.scss"
 //import beerTypes from "../../Types/BeerTypes";
 
 type BeerCardProps = {
   name: string,
   imageUrl: string,
-  description: string
+  description: string,
+  styleVariant: string,
 };
 
 
-const BeerCard = ({name, imageUrl, description} : BeerCardProps) => {
+const BeerCard = ({name, imageUrl, description, styleVariant} : BeerCardProps) => {
 
+    const truncateDescription = () => {
+        const words = description.split(' ');
+        if (words.length > 12 ) {
+          return words.slice(0, 12).join(' ') + '...';
+        }
+        return description;
+    }
+
+const className = `beer-card beer-card--${styleVariant}`;
     return (
-        <div className = "beer-card">
-            <img className = "beer-card__image"
-            key ={name}
-            src={imageUrl} 
-            alt={name} />
-            <p className = "beer-card__name">{name}</p>
-            <p className = "beer-card__abrv-info" >{description}description</p>
+        <div className = {className}>
+            <div className = "beer-card__content">
+                <img className = "beer-card__image"
+                key ={name}
+                src={imageUrl} 
+                alt={name} />
+                <p className = "beer-card__name">{name}</p>
+                <p className = "beer-card__abrv-info" >{truncateDescription()}</p>
+            </div>
         </div>
     );
 
--- src/Components/BeerGallery/BeerGallery.tsx
+++ src/Components/BeerGallery/BeerGallery.tsx
@@ -1,6 +1,6 @@
 import {BeerTypes} from "../../Types/beerTypes";
 import BeerCard from "../BeerCard/BeerCard";
-import "./BeerGallery.scss"; 
+//import "./BeerGallery.scss"; 
 
 type BeerGalleryProps = {
     beers: BeerTypes[];
@@ -13,14 +13,15 @@ const filteredBeers = beers.filter(beer => beer.image_url);
 
         return(
             <div className = "beer-gallery">
+                <h2 className = "beer-gallery__heading">{heading}</h2>
                 {filteredBeers.map(beer => {
-                    return (
-                        <div className = "beer-gallery__content">
-                            <h2 className = "beer-gallery__heading">{heading}</h2>
+                    return (       
+                        <div className = "beer-gallery__content">                    
                             <BeerCard 
                                 name = {beer.name} 
                                 imageUrl = {beer.image_url}
                                 description = {beer.description}
+                                styleVariant = "mainPage"
                             />
                         </div>
                     );
