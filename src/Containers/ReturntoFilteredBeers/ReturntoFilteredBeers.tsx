f0b5d8a1064622a066d95211cb512aef5360c8a8
Emma Oyetey <emmaflookis@hotmail.com>
2024-04-03 21:42:26 +0100

    styled ReturntoFilteredBeers so that nav does not appear. Replaced with  option to start a new search

--- src/Containers/ReturntoFilteredBeers/ReturntoFilteredBeers.tsx
+++ src/Containers/ReturntoFilteredBeers/ReturntoFilteredBeers.tsx
@@ -1,7 +1,9 @@
-//import "./Home.scss";
+import "./ReturntoFilteredBeers.scss";
 //import beers from "../../Data/beers";
 import BeerGallery from "../BeerGallery/BeerGallery";
 import { BeerTypes } from "../../Types/beerTypes";
+import { Link } from "react-router-dom";
+import cricketTest from "../../Assets/cricketTest.png";
 //import Nav from "../../Components/Nav/Nav";
 //import {filteredBeers} from "App.tsx" ;
 
@@ -14,9 +16,18 @@ const ReturntoFilteredBeers = ({ filteredBeers }: ReturntoBeersProps) => {
   return (
     <div className="filtered-beers__container">
       <div>
-        <button className="filtered-beers__button">Start a New Search?</button>
+        <button className="filtered-beers__button">
+          Start a New Search?
+              <Link to="/" className="refresh to all beers">
+            <img
+              className="filtered-beers__cricket"
+              src={cricketTest}
+              alt="cricket image"
+            />
+          </Link>
+          </button>
       </div>
-      <div>
+      <div className="filtered-beers__beer-gallery">
         <BeerGallery beers={filteredBeers} heading="Your Selected Beers" />;
       </div>
     </div>
