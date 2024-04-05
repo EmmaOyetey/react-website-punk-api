a7343d6b0058505159cccf4a6f96ecbc68b6eebc
Emma Oyetey <emmaflookis@hotmail.com>
2024-04-05 11:00:27 +0100

    removed some code to eliminate unsorted bugs

--- src/Containers/Home/Home.tsx
+++ src/Containers/Home/Home.tsx
@@ -2,10 +2,7 @@ import BeerGallery from "../BeerGallery/BeerGallery";
 import Nav from "../../Components/Nav/Nav";
 import beersData from "../../Data/beers";
 import { BeerTypes } from "../../Types/beerTypes";
-//import { useState } from "react";
 import "./Home.scss";
-//import {Link} from "react-router-dom";
-
 
 type HomeProps = {
     filteredBeers: BeerTypes[];
@@ -15,9 +12,6 @@ type HomeProps = {
 
 const Home = ({filteredBeers, handleFilterByKey, handleFilterByName}: HomeProps) => {
 
-    const noMatchingBeers = filteredBeers.length === 0;
-    const hasFilterApplied = filteredBeers.length !== beersData.length;
-
     return( 
         <section className="main">
             <section className="main__navigation">
@@ -25,18 +19,9 @@ const Home = ({filteredBeers, handleFilterByKey, handleFilterByName}: HomeProps)
                 handleFilterByName={handleFilterByName}
                 handleFilterByKey={handleFilterByKey}
                 allBeers={beersData}
-            />
             </section>
-            <section className = "main__beer-gallery">
-                {noMatchingBeers && hasFilterApplied ? (
-                    <section className ="main__beer-gallery--nobeers">
-                        <p>Sorry! No Beers matched that search..</p>
-                        <p>Please try again by clearing your search, or</p>
-                        <p>use the CRICKET to go back to the home page!</p>
-                    </section>
-                ) : (
-                    <BeerGallery beers={filteredBeers} heading="" />
-                )}
+            <section className = "Main__beer-gallery">
+            <BeerGallery beers = {filteredBeers} heading = ""/> 
             </section>
         </section >
 
